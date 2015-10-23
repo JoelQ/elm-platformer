ef9e937a856f24ae0564b0bb6bc108fc01068abe
Joël Quenneville <joelq@thoughtbot.com>
2015-10-23 12:53:51 +0100

    Add player entity and gravity
    This renders a character on canvas 30 times a second and applies
    gravity. The character can move right/left via arrow keys and jump with
    the space bar.

---
+++ Platformer.elm
@@ -0,0 +1,94 @@
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import Color exposing (..)
+import Keyboard
+import Time
+import Debug
+
+-- MODEL
+
+type alias Model =
+  { vx : Float
+  , vy: Float
+  , x : Float
+  , y : Float
+  , width : Float
+  , height : Float
+  }
+
+initialModel : Model
+initialModel =
+  { vx = 0
+  , vy = 0
+  , x = 0
+  , y = 0
+  , width = 50
+  , height = 100
+  }
+
+-- UPDATE
+
+type Action = Left | Right | Jump | Noop
+
+update : Action -> Model -> Model
+update action model =
+  accelerate action model
+    |> gravity
+    |> applyMovement
+    |> Debug.watch "model"
+
+accelerate : Action -> Model -> Model
+accelerate action model =
+  case action of
+    Left -> { model | vx <- (-50) }
+    Right -> { model | vx <- 50 }
+    Jump -> { model | vy <- 500 }
+    Noop -> { model | vx <- 0 }
+
+gravity : Model -> Model
+gravity model =
+  { model | vy <- model.vy - (0.033 * 980) }
+
+applyMovement : Model -> Model
+applyMovement model =
+  { model
+    | x <- model.x + (0.033 * model.vx)
+    , y <- model.y + (0.033 * model.vy)
+  }
+
+-- VIEW
+
+view : Model -> Element
+view model =
+  collage 200 200
+    [ rect 200 200 |> filled green
+    , rect model.width model.height |> filled blue |> move (model.x, model.y)
+    ]
+
+-- Signals
+
+walking : Signal Action
+walking =
+  Signal.map arrowToAction Keyboard.arrows
+
+arrowToAction : { x : Int, y : Int } -> Action
+arrowToAction arrow =
+  case arrow.x of
+    1 -> Right
+    (-1) -> Left
+    _ -> Noop
+
+jumps : Signal Action
+jumps =
+  Signal.map (\jump -> if jump then Jump else Noop) Keyboard.space
+
+actions : Signal Action
+actions =
+  Signal.merge walking jumps |> Signal.sampleOn (Time.fps 30)
+
+models : Signal Model
+models =
+  Signal.foldp update initialModel actions
+
+main : Signal Element
+main = Signal.map view models
